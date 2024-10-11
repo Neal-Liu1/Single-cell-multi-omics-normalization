19a03cd54f7e93a9d667f4ba236051404e014728
Neal Liu <neal.liu18@gmail.com>
2024-10-11 16:22:14 +1100

    added fast log transform for sparse matrices and fast celltype averaging for prps for sparse matrices

--- testPackage/R/Sc_helper_functions.R
+++ testPackage/R/Sc_helper_functions.R
@@ -1,5 +1,7 @@
 
-#' 
+#' @useDynLib testPackage, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+NULL
 
 #' Run all current popular RNA normalizations
 #' @description Runs Seurat logNormalized, Seurat CCA, Seurat SCT, fastMNN and Harmony, 
@@ -825,7 +827,7 @@ setMethod('ComputeMultipleLISI',
             
             LISI_scores <- lapply(reduction_matrix_list,
                                   function(x){
-                                    compute_lisi(
+                                    lisi::compute_lisi(
                                       x,
                                       meta_data = metadata,
                                       label_colnames = variables)})
@@ -1155,4 +1157,59 @@ py_umap <- function(matrix, neighbors = 30, min_dist = 0.01, n_components = 2, m
   embeddings <- reticulate::py_to_r(embeddings)
   return(embeddings)
 }
+
+
+
+# Fast log2 transform for sparse matrices
+log2_sparse <- function(matrix, pseudocount = 1){
+  require(Matrix)
+  if(!is(matrix, "sparseMatrix")){stop('Your matrix is not a sparse matrix.')}
+  matrix@x <- log2(matrix@x + pseudocount)
+  matrix <- Matrix::drop0(matrix)
+  return(matrix)
+}
+
+# Fast averaging for sparse matrices
+average_celltypes_sparse <- function(expr_sparse, batch_info, celltype_info, min_cells = 3) {
+  require(Matrix)
+  # Create an index to identify unique (celltype, batch) pairs
+  group_indices <- paste(celltype_info, batch_info, sep = ".")
+  # Initialize a sparse matrix to store the sums
+  gene_names <- rownames(expr_sparse)
+  unique_groups <- unique(group_indices)
+  
+  sum_matrix <- Matrix(0, nrow = nrow(expr_sparse), ncol = length(unique_groups), sparse = TRUE)
+  colnames(sum_matrix) <- unique_groups
+  rownames(sum_matrix) <- gene_names
+  # Loop over each group to sum the expression values
+  for (i in seq_along(unique_groups)) {
+    group <- unique_groups[i]
+    group_cells <- which(group_indices == group)
+    if (length(group_cells) < min_cells){next}
+    sum_matrix[, i] <- rowSums(expr_sparse[, group_cells, drop = FALSE])
+  }
+  
+  # Create a vector of cell counts per group
+  cell_counts <- table(group_indices)
+  cell_counts <- cell_counts[colnames(sum_matrix)]
+  
+  # Divide the sums by the cell counts to get the averages
+  avg_matrix <- sum_matrix %*% Diagonal(x = 1 / as.numeric(cell_counts))
+  
+  #celltype_names <- sapply(colnames(sum_matrix), function(x){unlist(strsplit(x, '[.]'))[1]})
+  #colnames(avg_matrix) <- celltype_names
+  colnames(avg_matrix) <- colnames(sum_matrix)
+  avg_matrix <- as.matrix(avg_matrix)
+  
+  if(length(which(colSums(avg_matrix) == 0)) == 0)
+  {return(avg_matrix)}
+  
+  avg_matrix <- avg_matrix[,-which(colSums(avg_matrix) == 0)]
+  
+  return(avg_matrix)
+}
+
+
+
+
 
