fd60db422779657c8c32110b8dcfc0ff93e7922b
Neal Liu <neal.liu18@gmail.com>
2024-10-31 02:04:36 +1100

    added benchmarkmetrics method for findncgs function

--- testPackage/R/Sc_helper_functions.R
+++ testPackage/R/Sc_helper_functions.R
@@ -780,6 +780,53 @@ setMethod('FindNCG',
           })
 
 
+setMethod('FindNCG', 
+          signature = c(object = 'BenchmarkMetrics',
+                        unwanted_variables = 'character',
+                        bio_variables = 'character'),
+          function(object,
+                   unwanted_variables,
+                   bio_variables,
+                   no.ncg,
+                   apply_log,
+                   sample_fraction){
+            
+            sample_num <- sample_fraction * ncol(object@Raw_data)
+            sample_ <- sample(ncol(object@Raw_data), sample_num)
+            message(paste0('Sampling ', sample_num,' cells from your data'))
+            
+            # Hardcoding main data matrix to assays > RNA > layers > counts
+            if(apply_log){data <- log2_sparse(object@Raw_data, pseudocount = 1)}
+            else{data <- object@Raw_data}
+            
+            data <- data[,sample_] %>% as.matrix()
+            metadata <- object@Metadata[sample_,]
+            
+            message('Calculating Spearman correlation for continuous variables & F score for categorical variables')
+            corr_data <- lapply(c(unwanted_variables, bio_variables), 
+                                            function(x){find_corr(data, metadata[[x]])})
+            
+            names(corr_data) <- c(unwanted_variables, bio_variables)
+            
+            # Flip bio variables 
+            message('Finding genes highly affected by unwanted variables but not affected by biological variables')
+            for(name in names(corr_data)){
+              if(name %in% bio_variables){
+                corr_data[[name]] <- corr_data[[name]] * (-1)
+              }
+            }
+            corr_data <- do.call(cbind, corr_data) %>% as.data.frame()
+            rownames(corr_data) <- rownames(object@Raw_data)
+            ranks <- apply(corr_data, 2, rank) %>% as.data.frame()
+            ranks$avg_expr <- rowMeans2(data)
+            prod_ranks <- apply(ranks, 1, prod)
+            final_gene_ranks <- prod_ranks[base::order(-prod_ranks)]
+            message('FindNCG Completed!')
+            
+            return(names(final_gene_ranks)[1:no.ncg])
+            
+          })
+
 
 
 # The benchmark metrics object 
